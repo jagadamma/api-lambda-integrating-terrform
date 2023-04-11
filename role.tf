a9597272c2905da77b6877611ef0998d49936638
jagadamma <96439002+jagadamma@users.noreply.github.com>
2023-04-11 21:18:38 +0530

    Create role.tf

---
+++ role.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+# Attach IAM policy to role
+resource "aws_iam_role_policy_attachment" "lambda_role_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_role.name
+}
+
+# Add permission to Lambda function
+resource "aws_lambda_permission" "test_lambda_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+}
